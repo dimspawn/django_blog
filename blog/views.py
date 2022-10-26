57bc0b31113d25ea71bae6b59d902e6c75e82d3a
Dimas Panuji Wicaksono <dimaswicaksono17@gmail.com>
2022-10-27 00:26:24 +0700

    next commit

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from django.views import View
 
 # Create your views here.
+
+class StartingPageView(ListView):
+    pass
+
+class AllPostsView(ListView):
+    pass
+
+class SinglePostView(View):
+    pass
+
+class ReadLaterView(View):
+    pass
